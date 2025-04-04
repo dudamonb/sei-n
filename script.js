d3fe432fa5d6f96d5ab464e619ac544a24acc548
duda <dudamonb@gmail.com>
2025-04-04 12:45:18 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 const fs = require('fs')
+const { cachedDataVersionTag } = require('v8')
 
 operation()
 
@@ -23,15 +24,125 @@ function operation(){
        ]). then((answer) => {
         const action = answer['action']  //receber a respota a partir da ação do usuário
 
-        if(action === 'Criar Conta'){ 
+        if(action === 'Criar Conta'){ // CONDICIO DA AÇÃO 
             createAccount()
+            buildAccount()
         }
-       })
-       .catch(err => console.log(err))  
+        else if(action === 'Depositar'){
+            deposit()
+        }
+        else if(action === 'Depositar'){
+            deposit()
+
+        }else if(action === 'Consultar Saldo'){
+            getAccountBalance()
+        }
+        else if(action === 'Sacar'){
+            withdraw()
+
+        }else if(action === 'Sair'){
+            console.log(chalk.bgBlue.black('Obrigado por utilizar o Liso Banking'))
+            process.exit
+        }
+           
+        })
+
+            .catch(err => console.log(err))  // VERIFIFCAR OS ERROS E CORRIGIR-LOS
      }
 
      function createAccount(){
-        console.log(chalk.bgGreen.black('Parabéns por escolher o Liso Banking!'))
-        console.log(chalk.green('Define as opções de sua conta a seguir!'))
+        console.log(chalk.bgGreen.black('Parabéns por escolher o Liso Banking!')) // informações da conta
+        console.log(chalk.green('Define as opções de sua conta a seguir!')) // informações da conta
      }
+     
+     // código de criar uma conta em si // 
+     function buildAccount() {
+        inquirer.prompt([
+            {
+                name:'accountName',
+                message: 'Digite um nome para sua conta:',
+            },
+        ]).then((answer) => {
+            const accountName = answer['accountName']
+            console.info(accountName)
+
+            if(!fs.existsSync('accounts')){
+                fs.mkdirSync('accounts')
+            }
+
+            if(fs.existsSync(`accounts/${accountName}.json`)){  // verificar a existência de forma síncrona = esquerda para direita, de cima para baixo  
+                console.log(chalk.bgRed.black('Esta conta já existe, insira outro nome.'))
+
+
+                buildAccount()
+                return
+
+            }
+            fs.writeFileSync(
+                `accounts/${accountName}.json`, '("balance: 0")',
+                function(err){
+                    console.log(err)
+                },
+            )
+            console.log(chalk.green('Parabéns! você tem mais uma conta com saldo'))
+            operation()     
+        }).catch((err) => console.log(err))
+
+     }
+
+     function checkAccount(accountName){
+        if(!fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Conta inexistente, tente novamente'))
+            return false
+
+        }
+        return true
+     }
+
+     function addAmount(accountName, amount){
+        const accountData = getAccount(accountName)
+
+        if(!amount){
+            console.log(cachedDataVersionTag)
+        }
+     }
+
+     
+
+     // criar arquivo para armazenar informação 
+
+     function deposit(){
+        inquirer.prompt([
+        {
+            name:'accountName',                          // nome da conta
+            message: 'Qual o nome da sua conta?'
+        }
+       ]).then((answer) =>{
+        const accountName = answer['accountName']
+
+        if(!checkAccount(accountName)){
+            return deposit()
+        } 
+
+        inquirer.prompt([
+            {
+                name:'amount',
+                message: 'Digite o valor do depósito',
+
+            }
+
+            .then((answer)=>{
+                const amount = answer['amount']
+                addAmount(accountName,amount)
+                operation()
+
+            })
+            .catch((err))
+          
+        ])
+
+       }).catch()
+
+     }
+
      
